3728e850ba1af0e169e5107b83ee08bb89c08daf
badzianga <tomekoszczypko@gmail.com>
2022-05-26 17:54:37 +0200

    Added HUD and player flip

--- libs/controller.py
+++ libs/controller.py
@@ -1,17 +1,35 @@
-import pygame
+from pygame import Surface, SRCALPHA
+from pygame.font import Font
 
 from .player import Mariusz
 from .constants import WHITE
 
 
 class Controller:
-    def __init__(self, screen: pygame.Surface) -> None:
+    def __init__(self, screen: Surface) -> None:
         self.screen = screen
-        self.font = pygame.font.Font("fonts/PressStart2P.ttf", 8)
+        self.font = Font("fonts/PressStart2P.ttf", 8)
 
         self.player = Mariusz(self.screen, 32, 32)
 
+        self.hud = Surface((224, 16), SRCALPHA)
+        self.hud_components = {
+            "MARIUSZ": (8, 0),
+            "000000": (8, 8),
+            "x00": (80, 8),
+            "WORLD": (128, 0),
+            "1-1": (128 , 8),
+            "TIME": (184, 0),
+            "400": (184, 8)
+        }
+        self.update_hud()
+
+    def update_hud(self):
+        for text, pos in self.hud_components.items():
+            surf = self.font.render(text, False, WHITE)
+            self.hud.blit(surf, pos)
+
     def run(self, dt: float) -> None:
-        surf = self.font.render("MARIUSZ", False, WHITE)
         self.player.update(dt)
-        self.screen.blit(surf, (32, 16))
+
+        self.screen.blit(self.hud, (16, 8))
--- libs/player.py
+++ libs/player.py
@@ -1,9 +1,10 @@
+from pygame.constants import K_DOWN, K_LEFT, K_RIGHT, K_x, K_z
 from pygame.image import load as load_image
+from pygame.key import get_pressed
 from pygame.math import Vector2
 from pygame.sprite import Sprite
 from pygame.surface import Surface
-from pygame.key import get_pressed
-from pygame.constants import K_LEFT, K_RIGHT, K_UP, K_DOWN, K_z, K_x
+from pygame.transform import flip
 
 
 class Mariusz(Sprite):
@@ -13,20 +14,22 @@ class Mariusz(Sprite):
         self.screen = screen
 
         self.image = load_image("img/idle_0.png").convert_alpha()
+        self.flip = False
+
         self.rect = self.image.get_rect(topleft=(x, y))
         self.pos = Vector2(x, y)
         self.speed = Vector2(0, 0)
 
-    def draw(self):
-        self.screen.blit(self.image, self.rect)
-
-    def update(self, dt: float):
+    def move_horizontally(self, dt: float):
         keys = get_pressed()
 
         if keys[K_LEFT]:
-            self.speed.x = max(self.speed.x - 0.1 * dt, -2)
+            self.speed.x = max(self.speed.x - 0.2 * dt, -2)
+            self.flip = True
         if keys[K_RIGHT]:
-            self.speed.x = min(self.speed.x + 0.1 * dt, 2)
+            self.speed.x = min(self.speed.x + 0.2 * dt, 2)
+            self.flip = False
+
         if not keys[K_LEFT] and not keys[K_RIGHT]:
             if self.speed.x > 0.2:
                 self.speed.x -= 0.1 * dt
@@ -36,5 +39,14 @@ class Mariusz(Sprite):
                 self.speed.x = 0
         self.pos.x += self.speed.x * dt
         self.rect.x = self.pos.x
+
+    def draw(self):
+        if self.flip:
+            self.screen.blit(flip(self.image, True, False), self.rect)
+        else:
+            self.screen.blit(self.image, self.rect)
+
+    def update(self, dt: float):
+        self.move_horizontally(dt)
 
         self.draw()
