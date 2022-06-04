9f12a40aeb4b070a6d533cb39fbbb98dfcdda725
badzianga <tomekoszczypko@gmail.com>
2022-06-04 21:35:34 +0200

    Added block bump animation

--- libs/tiles.py
+++ libs/tiles.py
@@ -13,6 +13,9 @@ class Tile(Sprite):
         self.image = image
         self.rect = self.image.get_rect(topleft=position)
 
+    def update(self) -> None:
+        pass
+
     def bump(self) -> None:
         return
 
@@ -25,6 +28,7 @@ class QuestionBlock(Sprite):
             load_image(f'img/question_block_{i}.png').convert_alpha()
             for i in range(3)
         ]
+        self.images.append(load_image('img/plate_0.png').convert_alpha())
         self.frame = 0
         self.animation = ((0, 0.45), (1, 0.15), (2, 0.15), (1, 0.15))
         self.last_time = time()
@@ -33,17 +37,35 @@ class QuestionBlock(Sprite):
         self.rect = self.image.get_rect(topleft=position)
 
         self.sound = Sound('sfx/smb_bump.wav')
+        self.bumped = False
+        self.just_bumped = False
 
     def update(self) -> None:
-        if time() - self.last_time >= self.animation[self.frame][1]:
+        if self.bumped:
+            return
+        if self.just_bumped:
+            if time() - self.last_time >= 0.015:
+                if self.frame < 5:
+                    self.rect.y -= 1
+                elif self.frame < 10: 
+                    self.rect.y += 1
+                else:
+                    self.bumped = True
+                self.frame += 1
+                self.last_time = time()
+            
+        elif time() - self.last_time >= self.animation[self.frame][1]:
             self.last_time = time()
             self.frame += 1
             if self.frame >= 4:
                 self.frame = 0
-        self.image = self.images[self.animation[self.frame][0]]
+            self.image = self.images[self.animation[self.frame][0]]
 
     def draw(self, screen: Surface) -> None:
         screen.blit(self.image, self.rect)
 
     def bump(self) -> None:
         self.sound.play()
+        self.image = self.images[3]
+        self.just_bumped = True
+        self.frame = 0
