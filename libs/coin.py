6d21222f9649835082369b12e49a5227b609b3e6
badzianga <tomekoszczypko@gmail.com>
2022-06-06 18:47:31 +0200

    Added comments to coin.py

--- libs/coin.py
+++ libs/coin.py
@@ -1,13 +1,18 @@
 from time import time
+
+from pygame.image import load as load_image
 from pygame.sprite import Sprite
 from pygame.surface import Surface
-from pygame.image import load as load_image
 
 
 class Coin(Sprite):
-    def __init__(self, position, theme) -> None:
+    """Animated static Coin object visible on map."""
+
+    def __init__(self, position: tuple, theme: str) -> None:
+        """Initialize Coin object."""
         super().__init__()
 
+        # animation and visual stuff
         self.images = [
             load_image(f'img/{theme}/coin_{i}.png').convert_alpha()
             for i in range(3)
@@ -15,11 +20,13 @@ class Coin(Sprite):
         self.frame = 0
         self.animation = ((0, 0.45), (1, 0.15), (2, 0.15), (1, 0.15))
         self.last_time = time()
-
         self.image = self.images[0]
+
+        # position
         self.rect = self.image.get_rect(topleft=position)
 
     def update(self, screen: Surface) -> None:
+        """Update animation and image, draw coin onto screen."""
         if time() - self.last_time >= self.animation[self.frame][1]:
             self.last_time = time()
             self.frame += 1
@@ -30,4 +37,5 @@ class Coin(Sprite):
         self.draw(screen)
 
     def draw(self, screen: Surface) -> None:
+        """Draw coin onto screen."""
         screen.blit(self.image, self.rect)
