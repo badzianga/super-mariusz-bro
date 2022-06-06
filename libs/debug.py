046366f552dbc72b0e40cb411d3b4d92c58fd046
badzianga <tomekoszczypko@gmail.com>
2022-06-06 19:45:20 +0200

    Added comments to debug.py

--- libs/debug.py
+++ libs/debug.py
@@ -6,11 +6,15 @@ from .constants import RED
 
 
 class Debug:
+    """Debug object used for displaying current FPS."""
+
     def __init__(self, screen: Surface, clock: Clock) -> None:
+        """Initialize Debug."""
         self.screen = screen
         self.clock = clock
         self.font = Font("fonts/PressStart2P.ttf", 8)
 
     def draw(self) -> None:
+        """Create Surface with current FPS and draw it onto screen."""
         surf = self.font.render(str(int(self.clock.get_fps())), False, RED)
         self.screen.blit(surf, (0, 0))
